a05a4ec133923ebe28c3b1030ebf82413b44574b
PradeepSamiappan <pradeep.samiappan@superawesome.com>
2023-10-16 14:49:33 +0100

    chore: tidyup

--- app/src/main/java/com/weather/WeatherForecastApplication.java
+++ app/src/main/java/com/weather/WeatherForecastApplication.java
@@ -1,15 +1,15 @@
 package com.weather;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
 public class WeatherForecastApplication {
 
     public static void main(String[] args) {
-        Environment env = SpringApplication.run(WeatherForecastApplication.class, args).getEnvironment();
+        SpringApplication.run(WeatherForecastApplication.class, args).getEnvironment();
     }
 }
